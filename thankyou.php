1973fb6b66210f8760c10de6416aa10dbb77ce8b
fullstackdevhasan <hasan.khan567@gmail.com>
2021-04-17 10:45:50 +0530

    Kr Mangalam Gurgoan

---
+++ thankyou.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="images/krmu-favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Private Engineering College in Delhi, NCR</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],    
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M8QBJT7');</script>
+    <!-- End Google Tag Manager -->
+</head>
+
+<body>
+     <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8QBJT7"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <section class="banner">
+        <div class="brand-logo position-relative">
+            <img src="images/kr_mangalam_university.png" alt="k r Mangalam">
+        </div>
+        <div class="header-content">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content-wrap border-0 p-lg-5">
+                        <h1 class="text-center font-weight-bold text-white">Thank you for contacting us.
+                            Our Admission Team will revert with the enquired details. </h1>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="form-container d-none">
+                        <div class="form-header">
+                            <div class="mobile_number">
+                                <a class="engineering_desktop" href="tel:+919311411717"><img width="22" height="22"
+                                        title="" alt=""
+                                        src="https://krmangalam.edu.in/campaigns/engineering/new/images/phone-icon.png">
+                                    +91-931-<span id="top_num2">View Number</span></a>
+                                <div class="whats_app">
+                                    <a href="https://web.whatsapp.com/send?phone=09311411717" target="_blank"><img
+                                            src="images/whatsapp-icon.png" alt="whats_app"></a>
+                                </div>
+
+                            </div>
+                            <h2 class="form-title">Admission Open
+
+                                <a href="#" class="cross_img"><img
+                                        src="https://krmangalam.edu.in/campaigns/engineering/new/images/cross.png"
+                                        class="img-fluid crossbtn"></a>
+                            </h2>
+                            <p class="session">For Session 2021-22</p>
+                        </div>
+                        <div class="npf_wgts" data-height="400px" data-w="90dc094b13624e97149378976e2e5a0d"></div>
+
+                    </div>
+
+                    <div class="mobile-cta">
+                        <ul>
+                            <li><a class="gear-2" href="javascript:void(0);">Apply Now <small>Session
+                                        2021-22</small></a><img src="images/up.png" alt="" class="mr-2"></li>
+                            <li><img src="images/phone-icon.png" alt="" class="ml-2"><a class="engineering_mobile"
+                                    href="tel:+919311411717">931-141-1717</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="courses-offers">
+        <div class="container">
+            <h2>Courses Offered</h2>
+            <p>Various graduate and postgraduate programmes offered in campus</p>
+            <div class="tabs-section">
+                <div class="tabs-header">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="graduate-program">Graduate Programmes</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="tab-inner-content">
+                            <h3 class="course-heading">B.Sc.(Hons.)</h3>
+                            <a href="" class="course-subheading">Agriculture <img
+                                    src="images/right-arrow.png" alt="icon"></a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="placement-highlight">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-lg-2">
+                    <div class="placement-left-wrapper">
+                        <div class="placement-icon">
+                            <img src="images/icon-1.png" alt="placement" class="img-fluid">
+                        </div>
+                        <span class="package">Package</span>
+                        <span class="avgpkg">Avg. Package of</span>
+                        <span class="price">Rs.<strong>6L</strong></span>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="placement-right-wrapper">
+                        <h3>Placement <br> Hightlight 2020</h3>
+                        <p>Various graduate and postgraduate programmes offered in campus</p>
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="counter-number">200+</div>
+                                <div class="counter-text">Corporate visited</div>
+                                <div class="counter-text">Hightest Package of</div>
+                                <span class="price">Rs.<strong>24L</strong></span>
+                            </div>
+                            <div class="col-md-7">
+                                <img src="images/placement-image.jpg" alt="placement" class="img-fluid m-tm-2">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="about-us-content">
+                        <h4>About K.R. Mangalam Group</h4>
+                        <p>An eternal appetite for the expansion of valuable knowledge has brought the
+                            K.R. Mangalam a long way.</p>
+                        <p>Almost 2 decades ago, the journey of educational excellence began with a world-class
+                            school at a pre-primary level in Greater Kailash - II. Today we have
+                            prospered into an innovative learning setup with post-graduate
+                            establishments into different geographies.
+                        </p>
+                        <div class="facts-and-figure">Facts and Figures</div>
+                        <div class="counters-wrap d-flex">
+                            <div class="number">08+ <br> <span>Schools</span></div>
+                            <div class="number">01 <br> <span>University</span></div>
+                        </div>
+                        <div class="stablished d-md-none">
+                            <small>ESTABLISHED IN</small>
+                            <strong>2003</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="campus-life">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h3>Campus <br> Life</h3>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="content-box hostel">
+                        <div class="icon">
+                            <img src="images/campus-hostel.png" alt="" class="img-fluid">
+                        </div>
+                        <h5>In-Campus Hostel <br> Facility</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="content-box seperatehostel">
+                        <div class="icon">
+                            <img src="images/seperate-hostel.png" alt="" class="img-fluid">
+                        </div>
+                        <h5>Separate Hostels for <br> Boys & Girls</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="content-box hygienicfood">
+                        <div class="icon">
+                            <img src="images/hygienic-food.png" alt="" class="img-fluid">
+                        </div>
+                        <h5>Serves Homely, Wholesome, Nourishing & Hygienic Food</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="content-box fastfood">
+                        <div class="icon pt-4">
+                            <img src="images/fast-food.png" alt="" class="img-fluid">
+                        </div>
+                        <h5>Multi Culinary Menu & Fast Food</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="content-box attachedbath">
+                        <div class="icon pt-4">
+                            <img src="images/attached-bath.png" alt="" class="img-fluid">
+                        </div>
+                        <h5>2/3 Seater A/C, Non-A/C Fully Furnished Rooms with Attached Bath</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="content-box sanatize">
+                        <div class="icon pt-4">
+                            <img src="images/sanatize.png" alt="" class="img-fluid">
+                        </div>
+                        <h5>High Standards of Hygiene and Sanitation</h5>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="content-box wifi">
+                        <div class="icon pt-4">
+                            <img src="images/wi-fi.png" alt="" class="img-fluid">
+                        </div>
+                        <h5>24x7 Wi-Fi Facility <br> Available</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="recruiter-testimonials">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="recruiter">
+                        <h3>Our Recruiters</h3>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy</p>
+                        <div class="owl-carousel owl-theme" id="logo-slider">
+                            <div class="image-wrapper">
+                                <div class="carousel-image-control"><img src="images/fedex.png" alt="fedex">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/religare.png" alt="religare">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/kotak.png" alt="kotak">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/techmahindra.png"
+                                        alt="techmahindra">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/hindustan-times.png"
+                                        alt="hindustan-times">
+                                </div>
+                            </div>
+                            <div class="image-wrapper">
+                                <div class="carousel-image-control"><img src="images/bajaj-motors.png"
+                                        alt="bajaj motors">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/bureau.png" alt="bureau">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/convergeys.png" alt="convergeys">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/byjus.png" alt="byjus">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/genpact.png" alt="genpact">
+                                </div>
+
+                            </div>
+                            <div class="image-wrapper">
+                                <div class="carousel-image-control"><img src="images/american-express.png"
+                                        alt="american express">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/jaro-education.png"
+                                        alt="jaro education">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/just-dial.png" alt="just dial">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/karvy.png" alt="karvy">
+                                </div>
+                                <div class="carousel-image-control"><img src="images/tata-capital.png"
+                                        alt="tata capital">
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="testimonials">
+                        <h3>Alumni - Testimonial</h3>
+                        <div class="testimonials-container">
+                            <div class="testimonials-content">
+                                <p>It was my immense luck and fortune to be the part of KRMU where I can grow. The
+                                    entire faculty and department leaves no stone unturned to shape one's future. My
+                                    four years at KRMU have been a wonderful experience of learning with prolific
+                                    exposure to outside. Huge respect, love and devotion for entire faculty members and
+                                    department. It's their efforts that make me to count myself into better
+                                    professionals.</p>
+                                <div class="author-name">Abhishek Sharma</div>
+                                <div class="batch">Batch 2018-20(M.Tech)</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="d-flex">
+        <div class="brand">K.R. Mangalam University</div>
+        <div class="address">Sohna Road, Gurugram 122103, Delhi-NCR</div>
+        <div class="ft-contact"><b>Helpline No.</b> :+91-931-141-1717</div>
+    </footer>
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+
+            $("#logo-slider").owlCarousel({
+                items: 1,
+                autoplay: true,
+                loop: true,
+                ltr: true,
+                margin: 25,
+                autowidth: true
+            });
+
+            $("#view_num").click(function () {
+                $("#view_num").text("141-1717");
+            });
+
+            $("#top_num2").click(function () {
+                $("#top_num2").text("141-1717");
+            });
+
+            $(".gear").click(function () {
+                $(".form-container").show();
+            });
+            $(".gear-2").click(function () {
+                $(".form-container").addClass("d-block");
+                $(".form-container").addClass("sticky-form-open");
+            });
+            $(".cross_img").click(function () {
+                $(".form-container").removeClass("d-block");
+                $(".form-container").removeClass("sticky-form-open");
+            });
+        });
+    </script>
+    <script type="text/javascript">
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://widgets.nopaperforms.com/emwgts.js";
+        document.body.appendChild(s);
+    </script>
+</body>
+
+</html>
